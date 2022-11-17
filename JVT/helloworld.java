ccb1b2abd06667e2d6468ab039acc6d7a43a0255
anon <abishkar.koirala@student.trinity.edu.np>
2022-11-17 12:27:20 +0545

    JVT

---
+++ JVT/helloworld.java
@@ -0,0 +1,11 @@
+package JVT;
+
+public class helloworld {
+    static String randomString = "String to print";
+    static final double PINUM = 3.14;
+
+    public static void main(String[] args) {
+        System.out.println(randomString);
+        System.out.println(PINUM);
+    }
+}
